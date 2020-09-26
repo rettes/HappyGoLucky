bd32fce51147d87bbb8b2446ec40b466e49e79f6
John Teo <john.teo.2018@sis.smu.edu.sg>
2020-09-26 11:05:48 +0800

    changes

--- codeitsuisse/routes/inventory-management.py
+++ codeitsuisse/routes/inventory-management.py
@@ -12,8 +12,8 @@ def evaluate2():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    print(inputValue)
-    logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return inputValue
+    # logging.info("My result :{}".format(result))
+    # return json.dumps(result);
 
 
